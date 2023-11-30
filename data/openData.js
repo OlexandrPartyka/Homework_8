8c6d24244f2e3bd368c027e8ffebc52f5221a7cd
OlexandrPartyka <144223043+OlexandrPartyka@users.noreply.github.com>
2023-11-30 20:12:52 +0200

    Update openData.js

--- data/openData.js
+++ data/openData.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 try{
-    const data = fs.readFileSync('C:/Users/negroinvalid/Desktop/ewrwerwer-main/data/personData.json', 'utf8');
+    const data = fs.readFileSync('file', 'utf8');
     console.log(data);
 } catch (err) {
     console.error('error while reading file', err.message);
